e307c767c68c3906bded513cf744c1b001acb50a
Smapifan <luca.vogel@gski.de>
2025-11-07 19:48:02 +0100

    Delete docs/Video Generator directory

--- docs/Video Generator/backend/controllers/videoController.js
+++
@@ -1,6 +0,0 @@
-exports.generateVideo = (req, res) => {
-  const { fps, volume, script, voices } = req.body;
-  // TODO: Logik für Video + Stimmen generieren
-  console.log('Video Daten:', fps, volume, script, voices);
-  res.json({ message: 'Video generation gestartet' });
-};
--- docs/Video Generator/backend/routes/videoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const videoController = require('../controllers/videoController');
-
-router.post('/generate', videoController.generateVideo);
-
-module.exports = router;
